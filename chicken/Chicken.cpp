f757b28420a2ee78666dc5e7266d3e0f60a24bad
zhuiguang <3230105121@zju.edu.cn>
2024-09-25 10:28:42 +0800

    feat:Lab 2 modification(version 2.0)

--- chicken/Chicken.cpp
+++ chicken/Chicken.cpp
@@ -1,96 +1,97 @@
 #include <iostream>
 #include <cstring>
 
-using namespace std;
-
 class Chicken
 {
-    private:
-        int age = 24;
-        char *name = nullptr;
-    public:
-        Chicken() : age(24), name(nullptr) {}
-        Chicken(int _age) : age(_age), name(nullptr) {}
-        Chicken(int _age, const char *_name) : age(_age) 
+private:
+    int age = 24;
+    char *name = nullptr;
+
+public:
+    Chicken() = default;
+
+    Chicken(int _age) : age(_age) {}
+
+    Chicken(int _age, const char *_name) : age(_age)
+    {
+        setName(_name); 
+    }
+    Chicken(const Chicken &other) : age(other.age)
+    {
+        if (other.name != nullptr)
         {
-            if (_name != nullptr) 
-            {
-                int len = strlen(_name) + 1;
-                name = new char[len];
-                strcpy(name, _name); 
-            }
+            setName(other.name);
         }
-        Chicken(const Chicken &other) : age(other.age) 
+    }
+    Chicken &operator=(const Chicken &other)
+    {
+        if (this == &other) 
+            return *this;
+        age = other.age;  
+        if (other.name != nullptr && (name != other.name))
         {
-            if (other.name != nullptr) 
-            {
-                int len = strlen(other.name) + 1;
-                name = new char[len];
-                strcpy(name, other.name);
-            }
-            else
-            {
-                name = nullptr;
-            }
+            setName(other.name);
         }
-        Chicken &operator=(const Chicken &other)
-        {
-            if (this == &other) 
-                return *this;
-            delete[] name;
-            age = other.age;
-            if (other.name != nullptr)
-            {
-                int len = strlen(other.name) + 1;
-                name = new char[len];
-                strcpy(name, other.name);
-            }
-            else
-            {
-            name = nullptr;
-            }
-            return *this; 
-        }
-        ~Chicken()
+        return *this;      
+    }
+    ~Chicken()
+    {
+        if (name != nullptr)
         {
             delete[] name;
         }
-        void setAge(int _age)
-        {
-            age = _age;
-        }
-        void setName(const char *_name)
+    }
+    void setName(const char *_name)
+    {
+        if (name != nullptr)
         {
             delete[] name; 
-            if (_name != nullptr)
-            {
-                int len = strlen(_name) + 1;
-                name = new char[len];
-                strcpy(name, _name);
-            }
-            else
-            {
-                name = nullptr;
-            }
         }
-        const char *getName() const
+
+        int len = strlen(_name) + 1;
+        name = new char[len];
+        for (int i = 0; i < len; i++)
         {
-            return name;
+            name[i] = _name[i];
         }
-        const int &getAge() const
-        {
-            return age;
-        }
+    }
+
+    const char *getName() const
+    {
+        return name;
+    }
+
+    const int &getAge() const
+    {
+        return age;
+    }
 };
 
 int main()
 {
-    Chicken c1(24, "Kunkun");
-    Chicken c2 = c1; 
-    Chicken c3;
-    c3 = c1; 
-    cout << "Hi, everyone! My name is " << c1.getName() << ", I am " << c1.getAge() << " years old." << endl;
-    cout << "Hi, everyone! My name is " << c2.getName() << ", I am " << c2.getAge() << " years old." << endl;
-    cout << "Hi, everyone! My name is " << c3.getName() << ", I am " << c3.getAge() << " years old." << endl;
+    auto print = [](const Chicken &c) {
+        std::cout << "Hi, everyone! My name is " << c.getName() << ", I am " << c.getAge() << " years old." << std::endl;
+    };
+
+    Chicken c(24, "Kunkun");
+    print(c);
+    
+    Chicken d;
+    d = c;
+    print(d);
+    
+    Chicken a = c;
+    print(a); 
+    
+    c.setName("Xukun Cai");
+    print(c);
+    print(a);
+    print(d);
+    
+    Chicken b;
+    b = d = c;
+    print(b);
+    print(d);
+
     return 0;
 }
